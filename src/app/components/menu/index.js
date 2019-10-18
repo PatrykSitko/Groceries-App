f4fa276a78671b8ea4de530c8eb0a305ffacc617
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2019-10-18 18:29:59 +0200

    Added entries to the menuFooter

--- src/app/components/menu/index.js
+++ src/app/components/menu/index.js
@@ -3,7 +3,8 @@ import { push } from "redux-first-routing";
 import { connect } from "react-redux";
 
 import MenuHeader from "./header";
-import MenuFooter from "./footer";
+import MenuFooter, { Entry } from "./footer";
+import images from "../../resources/images";
 
 const mapDispatchToProps = dispatch => {
   return { changePath: path => dispatch(push(path)) };
@@ -12,7 +13,11 @@ const mapDispatchToProps = dispatch => {
 function Menu({ changePath }) {
   return [
     <MenuHeader {...{ key: "header", title: "GROCERIES APP", changePath }} />,
-    <MenuFooter {...{ key: "footer", changePath }} />
+    <MenuFooter {...{ key: "footer", changePath }}>
+      <Entry title="List" image={{ src: images.menu.list, alt: "list" }} />
+      <Entry title="stats" image={{ src: images.menu.stats, alt: "stats" }} />
+      <Entry title="Recipes" image={{ src: images.menu.fork, alt: "fork" }} />
+    </MenuFooter>
   ];
 }
 
