3f2f7a867261de137f078452297b044a94c874f0
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2019-10-18 20:13:22 +0200

    Fixed menu header component position

--- src/app/components/menu/header.jsx
+++ src/app/components/menu/header.jsx
@@ -3,13 +3,21 @@ import "./header.scss";
 import LoginButton from "../button/login";
 
 function MenuHeader({ title, changePath, ...other }) {
-  return (
-    <header {...{ className: "menu-header", ...other }}>
+  return [
+    <header
+      {...{
+        key: "menu-header-background",
+        className: "menu-header-background"
+      }}
+    >
+      <div className="title">DRAWBOTICS</div>
+    </header>,
+    <header {...{ key: "menu-header", className: "menu-header", ...other }}>
       <div className="title">{title}</div>
       <div className="separator" />
       <LoginButton />
     </header>
-  );
+  ];
 }
 
 export default MenuHeader;
