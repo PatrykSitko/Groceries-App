28590a474eb507428fc9a00827a494093d07def1
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2019-10-18 17:07:35 +0200

    Styled the header component of the menu component

--- src/app/components/menu/header.jsx
+++ src/app/components/menu/header.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import "./header.scss";
+import LoginButton from "../button/login";
 
-function MenuHeader({ changePath }) {
-  return <div>Hello World!</div>;
+function MenuHeader({ title, changePath }) {
+  return (
+    <header className="menu-header">
+      <div className="title">{title}</div>
+      <div className="separator" />
+      <LoginButton />
+    </header>
+  );
 }
 
 export default MenuHeader;
