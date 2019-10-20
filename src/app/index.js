c60195431b467643ca661e70952a7d81f08afb29
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2019-10-20 17:10:20 +0200

    Mapped store routes to components

--- src/app/index.js
+++ src/app/index.js
@@ -1,12 +1,41 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import Home from "./routes/home";
+import { connect } from "react-redux";
 import "./app.scss";
+
+import Home from "./routes/home";
+import ListRoute from "./routes/list";
+import GraphRoute from "./routes/graph";
+import RecipesRoute from "./routes/recipes";
 
 document.title = "react app";
 
-export const Routes = () => (
+const routesComponents = {
+  list: ListRoute,
+  graph: GraphRoute,
+  recipes: RecipesRoute
+};
+
+const mapStateToProps = ({ state: { routes } }) => ({ routes });
+
+export const Routes = connect(mapStateToProps)(({ routes }) => (
   <Switch>
+    {Object.entries(routes).map(route)}
     <Route path="/" exact strict component={Home} />
   </Switch>
-);
+));
+
+/**
+ * @param {[language:string,routes:Object]}
+ * @returns {[Route]}
+ */
+function route([language, routes]) {
+  return Object.entries(routes).map(([key, route]) => (
+    <Route
+      path={`/${language}/${route}`}
+      exact
+      strict
+      component={routesComponents[key]}
+    />
+  ));
+}
