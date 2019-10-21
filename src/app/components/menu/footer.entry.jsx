029388e764a9f6ffc91e990a997c69a5a5298373
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2019-10-21 07:47:02 +0200

    Made the footer entry functional and more robust

--- src/app/components/menu/footer.entry.jsx
+++ src/app/components/menu/footer.entry.jsx
@@ -1,24 +1,41 @@
 import React, { useRef } from "react";
+import { connect } from "react-redux";
+import { push } from "redux-first-routing";
 
+const mapDispatchToProps = dispatch => ({
+  changePath: path => dispatch(push(path))
+});
 function MenuFooterEntry({
   image: { src, alt } = { src: "#", alt: "" },
   title,
+  enabled,
+  changePath,
   href
 }) {
   return (
-    <div className="entry" ref={useRef()}>
+    <div className="entry" ref={useRef()} onClick={() => changePath(href)}>
       <div className="image-container">
         {typeof src === "string" ? (
           <img {...{ src, className: "image" }} alt={alt} />
-        ) : typeof src === "object" ? (
-          React.cloneElement(src, { className: "image" })
+        ) : typeof src === "object" &&
+          typeof src.$$typeof === "symbol" &&
+          src.$$typeof.toString() === "Symbol(react.element)" ? (
+          React.cloneElement(src, {
+            className: `image${
+              src.props.className ? ` ${src.props.className}` : ""
+            }`,
+            enabled
+          })
         ) : (
           ""
         )}
       </div>
-      <p className="title">{title}</p>
+      <p className={`title${enabled ? " enabled" : ""}`}>{title}</p>
     </div>
   );
 }
 
-export default MenuFooterEntry;
+export default connect(
+  null,
+  mapDispatchToProps
+)(MenuFooterEntry);
