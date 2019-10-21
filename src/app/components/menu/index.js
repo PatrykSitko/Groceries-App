d086ec1ad771fa3b7f6817b2893d8b623cc3ed0f
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2019-10-21 07:51:08 +0200

    Made footer menu entries functional

--- src/app/components/menu/index.js
+++ src/app/components/menu/index.js
@@ -6,31 +6,65 @@ import MenuHeader from "./header";
 import MenuFooter, { Entry } from "./footer";
 import images from "../../resources/images";
 
-const mapDispatchToProps = dispatch => {
-  return { changePath: path => dispatch(push(path)) };
-};
+const mapStateToProps = ({
+  state: {
+    routes,
+    user: { language }
+  },
+  router
+}) => ({ routes, language, router });
+const mapDispatchToProps = dispatch => ({
+  changePath: path => dispatch(push(path))
+});
 
-function Menu({ changePath }) {
+function Menu({ routes, language, router, changePath }) {
+  const path = {
+    list: `/${language}/${routes[language].list}`,
+    graph: `/${language}/${routes[language].graph}`,
+    recipes: `/${language}/${routes[language].recipes}`
+  };
   return [
-    <MenuHeader {...{ key: "header", title: "GROCERIES APP", changePath }} />,
+    <MenuHeader
+      {...{
+        key: "header",
+        title: "GROCERIES APP",
+        image: { src: images.menu.grocery, alt: "grocery" },
+        changePath
+      }}
+    />,
     <MenuFooter {...{ key: "footer", changePath }}>
       <Entry
         title="List"
-        image={{ src: <images.interactive.menu.List />, alt: "list" }}
+        image={{
+          src: <images.interactive.menu.List />,
+          alt: "list"
+        }}
+        enabled={router.pathname.includes(path.list)}
+        href={path.list}
       />
       <Entry
         title="stats"
-        image={{ src: <images.interactive.menu.Stats />, alt: "stats" }}
+        image={{
+          src: <images.interactive.menu.Stats />,
+          alt: "stats"
+        }}
+        enabled={router.pathname.includes(path.graph)}
+        href={path.graph}
       />
       <Entry
         title="Recipes"
-        image={{ src: <images.interactive.menu.Fork />, alt: "fork" }}
+        image={{
+          src: <images.interactive.menu.Fork />,
+          alt: "fork"
+        }}
+        enabled={router.pathname.includes(path.recipes)}
+        href={path.recipes}
       />
     </MenuFooter>
   ];
 }
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(Menu);
