f38a7f23e3a3ad4dbf0502ba10eacc1e295ba3f9
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2019-10-21 15:22:34 +0200

    Binded router listener to store

--- src/redux/store/listeners/bind.js
+++ src/redux/store/listeners/bind.js
@@ -1,7 +1,9 @@
 import bindWindowListeners from "./listener/window";
 import bindHistoryListener from "./listener/history";
+import bindRouterListener from "./listener/router";
 
 export default store => {
   bindHistoryListener(store);
+  bindRouterListener(store);
   bindWindowListeners(store);
 };
