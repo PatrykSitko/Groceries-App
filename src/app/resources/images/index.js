8b5c9d89769ae4e9027419bc2ad8c0b3aae1fd59
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2019-10-22 19:11:55 +0200

    Added a button image

--- src/app/resources/images/index.js
+++ src/app/resources/images/index.js
@@ -1,4 +1,10 @@
 import * as interactive_menu from "./interactive/menu";
 import * as menu from "./menu";
 import * as animated from "./animated";
-export default { interactive: { menu: interactive_menu }, menu, animated };
+import * as button from "./button";
+export default {
+  interactive: { menu: interactive_menu },
+  menu,
+  animated,
+  button
+};
