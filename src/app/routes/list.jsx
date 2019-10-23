9b6a35c4e5527a34f71f939890ecf85c9202ae86
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2019-10-23 11:20:42 +0200

    Mapped store state to list component

--- src/app/routes/list.jsx
+++ src/app/routes/list.jsx
@@ -1,13 +1,25 @@
 import React from "react";
+import { connect } from "react-redux";
 
-import List from "../components/list";
+import List, { Category } from "../components/list";
 
-function ListRoute() {
+const mapStateToProps = ({
+  state: {
+    user: { language },
+    food: { "selected-category-key": selectedCategoryKey, categories }
+  }
+}) => ({ language, selectedCategoryKey, categories });
+
+function ListRoute({ language, selectedCategoryKey, categories }) {
   return (
     <section id="list">
-      <List />
+      <List initiallySelectedCategoryKey={selectedCategoryKey}>
+        {[categories].flat(Infinity).map(({ key, title }) => (
+          <Category {...{ key, id: key, title: title[language] }} />
+        ))}
+      </List>
     </section>
   );
 }
 
-export default ListRoute;
+export default connect(mapStateToProps)(ListRoute);
