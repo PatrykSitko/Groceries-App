7c23e186c4ea406c49392b620131576661acc7fa
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2019-10-23 15:00:26 +0200

    Implemented setSelectedFoodCategoryKey action

--- src/app/routes/list.jsx
+++ src/app/routes/list.jsx
@@ -2,18 +2,32 @@ import React from "react";
 import { connect } from "react-redux";
 
 import List, { Category } from "../components/list";
+import { setSelectedFoodCategoryKey } from "../../redux/actions/all";
 
-const mapStateToProps = ({
-  state: {
+const mapStateToProps = ({ state }) => {
+  const {
     user: { language },
     food: { "selected-category-key": selectedCategoryKey, categories }
-  }
-}) => ({ language, selectedCategoryKey, categories });
-
-function ListRoute({ language, selectedCategoryKey, categories }) {
+  } = state;
+  return { state, language, selectedCategoryKey, categories };
+};
+const mapDispatchToProps = dispatch => ({
+  setSelectedCategoryKey: (state, key) =>
+    dispatch(setSelectedFoodCategoryKey({ state, key }))
+});
+function ListRoute({
+  language,
+  state,
+  setSelectedCategoryKey,
+  selectedCategoryKey,
+  categories
+}) {
   return (
     <section id="list">
-      <List initiallySelectedCategoryKey={selectedCategoryKey}>
+      <List
+        initiallySelectedCategoryKey={selectedCategoryKey}
+        getSelectedCategoryKey={key => setSelectedCategoryKey(state, key)}
+      >
         {[categories].flat(Infinity).map(({ key, title }) => (
           <Category {...{ key, id: key, title: title[language] }} />
         ))}
@@ -22,4 +36,7 @@ function ListRoute({ language, selectedCategoryKey, categories }) {
   );
 }
 
-export default connect(mapStateToProps)(ListRoute);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ListRoute);
