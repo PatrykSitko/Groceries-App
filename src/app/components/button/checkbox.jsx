35ec51623e486cdd50a97edf3d694b1e14161e54
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2019-10-24 15:03:22 +0200

    Finnished checkbox button look and feel

--- src/app/components/button/checkbox.jsx
+++ src/app/components/button/checkbox.jsx
@@ -1,8 +1,22 @@
 import React from "react";
 import "./checkbox.scss";
 
-function CheckboxButton() {
-  return <div className="checkbox"></div>;
+function CheckboxButton({ useState, onClick, className, ...other }) {
+  const [checked, setChecked] = useState;
+  return (
+    <div
+      className={`checkbox${checked ? " checked" : ""}${
+        typeof className === "string" ? ` ${className}` : ""
+      }`}
+      onClick={event => {
+        setChecked(!checked);
+        if (typeof onClick === "function") {
+          onClick(event);
+        }
+      }}
+      {...other}
+    />
+  );
 }
 
 export default CheckboxButton;
