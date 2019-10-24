6827f5c02cb4c1138864c7cba4a4b83b880eea5c
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2019-10-24 12:09:40 +0200

    Creating product look and feel

--- src/app/components/list/products/product.jsx
+++ src/app/components/list/products/product.jsx
@@ -1,8 +1,22 @@
 import React from "react";
 import "./products.scss";
 
-function Product({}) {
-  return <div></div>;
+import Checkbox from "../../button/checkbox";
+
+function Product({ className, title: product, ...other }) {
+  delete other.dispatch;
+  return (
+    <div
+      className={`product${
+        typeof className === "string" ? ` ${className}` : ""
+      }`}
+      {...other}
+    >
+      {product}
+      <div className="separator" />
+      <Checkbox />
+    </div>
+  );
 }
 
 export default Product;
