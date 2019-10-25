16c07ea7c9871d8aebc10a114f065d736e6ccb88
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2019-10-25 10:31:06 +0200

    Added ref to toggle button

--- src/app/components/button/toggle.jsx
+++ src/app/components/button/toggle.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./toggle.scss";
 
 function ToggleButton({ useState, className, onClick, ...other }) {
   const [toggled, setToggled] = useState;
   return (
     <div
+      ref={useRef()}
       className={`toggle-button${toggled ? " toggled" : ""}${
         typeof className === "string" ? ` ${className}` : ""
       }`}
