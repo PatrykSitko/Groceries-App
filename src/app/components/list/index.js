a3bd5e05707ee16d537b54be753e654a36747bcd
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2019-10-25 10:32:26 +0200

    Fixed width and selected category overflow issue

--- src/app/components/list/index.js
+++ src/app/components/list/index.js
@@ -1,7 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import "./list.scss";
 
+import { getElementRef, getElementRect } from "../tools/element";
+import { vmin } from "../tools/vscale";
 import ToggleButton from "../button/toggle";
 import AddButton from "../button/add";
 
@@ -20,6 +22,7 @@ function List({
   ...other
 }) {
   delete other.dispatch;
+  const [categoriesStyle, setCategoriesStyle] = useState({});
   const [selectedCategoryID, setSelectedCategoryID] = useState(
     initiallySelectedCategoryKey
   );
@@ -28,19 +31,50 @@ function List({
   const selectedCategory = categories.filter(
     ({ props: { id } }) => id === selectedCategoryID
   )[0];
+  useEffect(() => {
+    const currentCategoryElement = document.getElementById(selectedCategoryID);
+    const currentCategoryRef = getElementRef(currentCategoryElement);
+    const currentCategoriesStyle = { width: 0 };
+    if (currentCategoryRef && currentCategoryRef.current) {
+      const currentCategoryRect = getElementRect(currentCategoryRef);
+      if (currentCategoryRect) {
+        const { left, bottom, width } = currentCategoryRect;
+        currentCategoriesStyle.left = left;
+        currentCategoriesStyle.top = bottom;
+        currentCategoriesStyle.width += width - vmin(0.7);
+      }
+    }
+    const currentToggleButtonElement = document.getElementById(
+      "list-toggle-button"
+    );
+    const currentToggleButtonRef = getElementRef(currentToggleButtonElement);
+    if (currentToggleButtonRef && currentToggleButtonRef.current) {
+      const { width } = getElementRect(currentToggleButtonRef);
+      currentCategoriesStyle.width += width;
+    }
+    if (
+      Object.values(currentCategoriesStyle).join("") !==
+      Object.values(categoriesStyle).join("")
+    ) {
+      setCategoriesStyle(currentCategoriesStyle);
+    }
+  }, [categoriesStyle, selectedCategoryID, windowInnerDimensions]);
   return (
     <div className="list" {...other}>
       {React.cloneElement(selectedCategory, {
         className: `selected-category${
           selectedCategory.props.title.length > 20 &&
-          windowInnerDimensions.width <= 1003
+          categoriesStyle.width <= 434.3
             ? " overflow"
             : ""
         }`
       })}
-      <ToggleButton useState={toggleButtonState} />
+      <ToggleButton id="list-toggle-button" useState={toggleButtonState} />
       <AddButton />
-      <div className={`categories${isToggled ? "" : " hidden"}`}>
+      <div
+        className={`categories${isToggled ? "" : " hidden"}`}
+        style={categoriesStyle}
+      >
         {categories
           .filter(({ props: { id } }) => id !== selectedCategoryID)
           .map(category => {
