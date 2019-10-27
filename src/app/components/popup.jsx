18ae974719abd93e007ecb594fda8ab75a42107d
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2019-10-27 16:09:48 +0100

    Created a popup component

---
+++ src/app/components/popup.jsx
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from "react";
+import "./popup.scss";
+
+import { useDisplayFixedMapDimensions } from "./effects";
+
+function Popup({
+  useState: [display, setDisplay],
+  windowInnerDimensions,
+  children,
+  ...other
+}) {
+  const style = {
+    ...useDisplayFixedMapDimensions(windowInnerDimensions)
+  };
+  const { hidden, effectClass } = useOpacityEffect(display);
+  return (
+    <section
+      {...{
+        hidden,
+        id: "popup-background",
+        className: effectClass,
+        style,
+        onClick: ({ target }) =>
+          target.id === "popup-background" ? setDisplay(!display) : ""
+      }}
+    >
+      <div id="popup" {...other}>
+        {children}
+      </div>
+    </section>
+  );
+}
+
+function useOpacityEffect(display) {
+  const [hidden, setHidden] = useState(undefined);
+  const [effectClass, setEffectClass] = useState(undefined);
+  useEffect(() => {
+    if (!display && (effectClass || !hidden)) {
+      const effectTimeout = setTimeout(() => {
+        setEffectClass(undefined);
+        clearTimeout(effectTimeout);
+      }, 1);
+      const hiddenTimeout = setTimeout(() => {
+        setHidden(true);
+        clearTimeout(hiddenTimeout);
+      }, 500);
+    }
+    if (display && (!effectClass || hidden)) {
+      setHidden(false);
+      const effectTimeout = setTimeout(() => {
+        setEffectClass(display ? "visible" : undefined);
+        clearTimeout(effectTimeout);
+      }, 1);
+    }
+  }, [hidden, display, effectClass]);
+  return { hidden, effectClass };
+}
+export default Popup;
