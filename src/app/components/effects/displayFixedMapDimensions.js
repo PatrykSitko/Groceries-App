d097b434ccec1f400c897b9fd8862d9bb374dbaa
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2019-10-27 16:56:21 +0100

    Changed saved target rect dimensions parameters

--- src/app/components/effects/displayFixedMapDimensions.js
+++ src/app/components/effects/displayFixedMapDimensions.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { getElementRef, getElementRect } from "../tools/element";
 
+import ObjectIncludesObject from "../tools/objectIncludesObject";
+
 export function useDisplayFixedMapDimensions(
   windowInnerDimensions,
   element = document.getElementById("route")
@@ -9,13 +11,9 @@ export function useDisplayFixedMapDimensions(
   useEffect(() => {
     if (element) {
       const elementRef = getElementRef(element);
-      const { top, bottom, width, height } = getElementRect(elementRef);
-      if (
-        typeof bottom === "number" &&
-        Object.values([top, bottom, width, height]).join("") !==
-          Object.values(style).join("")
-      ) {
-        setStyle({ top, bottom, width, height });
+      const { top, left, width, height } = getElementRect(elementRef);
+      if (!ObjectIncludesObject(style, { top, left, width, height })) {
+        setStyle({ top, left, width, height });
       }
     }
   }, [element, style, setStyle, windowInnerDimensions]);
