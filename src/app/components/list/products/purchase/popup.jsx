a3a9fd170edf15d9ec5b1a4e4d0e9eefd5b6cbb1
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2019-10-31 05:17:11 +0100

    Added input field into the purchase popup

--- src/app/components/list/products/purchase/popup.jsx
+++ src/app/components/list/products/purchase/popup.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
+import InputField from "../../../input/field";
 
 function PurchasePopup() {
+  const [inputText, setInputText] = useState("");
   return (
-    <div id="popup" className="">
-      purchase
+    <div id="popup" className="purchase">
+      <InputField useState={[inputText, setInputText]} />
     </div>
   );
 }
