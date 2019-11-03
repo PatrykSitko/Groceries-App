2c6cfbfabb6bddfaa20b1edc7b61ed1631ddf822
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2019-11-03 22:00:09 +0100

    Made Input field functional

--- src/app/components/input/field.jsx
+++ src/app/components/input/field.jsx
@@ -1,18 +1,19 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./field.scss";
 
-const FUNCTION = 1;
 function InputField({ useState: inputState, ...other }) {
-  const setInputText = inputState[FUNCTION];
+  const [inputText, setInputText] = inputState;
   return (
     <form
       onSubmit={e => e.preventDefault()}
       onChange={e => setInputText(e.target.value)}
     >
       <input
+        ref={useRef()}
         className="input-field"
         type="text"
         name="input-field"
+        defaultValue={inputText}
         {...other}
       />
     </form>
