c472cc2a001d0843625e77032f58ba13083478fb
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2019-11-09 11:02:30 +0100

    Commented out not yet working code

--- src/app/components/list/products/purchase/popup.jsx
+++ src/app/components/list/products/purchase/popup.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState /*useEffect*/ } from "react";
 import InputField from "../../../input/field";
 import { connect } from "react-redux";
 import List, { Category } from "../../../list";
