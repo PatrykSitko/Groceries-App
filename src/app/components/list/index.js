cb8f393c8f84075323b176076d5e66442e277860
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2019-11-09 11:03:15 +0100

    Removed React DOM import

--- src/app/components/list/index.js
+++ src/app/components/list/index.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useRef } from "react";
-import ReactDOM from "react-dom";
 import "./list.scss";
 
 import { getElementRef, getElementRect } from "../tools/element";
