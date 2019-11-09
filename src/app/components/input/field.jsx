269f82ad9656d106608f775e1302488d454ea1f6
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2019-11-09 14:55:54 +0100

    Fixed input field by removing faulty code

--- src/app/components/input/field.jsx
+++ src/app/components/input/field.jsx
@@ -1,33 +1,23 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useRef } from "react";
 import "./field.scss";
 
 function InputField({ filterKeys, useState: inputState, ...other }) {
   const [inputText, setInputText] = inputState;
-  const [currentInputText, setCurrentInputText] = useState("");
-  useEffect(() => {
-    const newCurrentInputText = currentInputText
-      .split("")
-      .filter(key => ![filterKeys].flat(Infinity).includes(key))
-      .join("");
-    if (newCurrentInputText !== currentInputText) {
-      setInputText(newCurrentInputText);
-      setCurrentInputText(newCurrentInputText);
-    }
-  }, [
-    filterKeys,
-    inputText,
-    setInputText,
-    currentInputText,
-    setCurrentInputText
-  ]);
   return (
     <input
       ref={useRef()}
       className="input-field"
       type="text"
       name="input-field"
-      onChange={e => setCurrentInputText(e.target.value)}
-      value={currentInputText}
+      onChange={e =>
+        setInputText(
+          e.target.value
+            .split("")
+            .filter(key => ![filterKeys].flat(Infinity).includes(key))
+            .join("")
+        )
+      }
+      value={inputText}
       {...other}
     />
   );
