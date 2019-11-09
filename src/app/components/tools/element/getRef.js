5c03f8b559dd542220509ea98b7123de462250c4
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2019-11-09 14:52:13 +0100

    Made getElementRef more error prone

--- src/app/components/tools/element/getRef.js
+++ src/app/components/tools/element/getRef.js
@@ -1,7 +1,14 @@
 const PROPS = 0;
 
 export function getElementRef(documentElement) {
-  return Object.values(documentElement)[PROPS].ref;
+  if (!documentElement) {
+    return;
+  }
+  const elementValues = Object.values(documentElement);
+  if (!typeof elementValues === "object") {
+    return;
+  }
+  return elementValues[PROPS] ? elementValues[PROPS].ref : undefined;
 }
 
 export default getElementRef;
