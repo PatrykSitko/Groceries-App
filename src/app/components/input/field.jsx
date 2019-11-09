5ed58f13aa0854b8648c69e825ad448f877187de
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2019-11-09 12:08:25 +0100

    Removed input lag

--- src/app/components/input/field.jsx
+++ src/app/components/input/field.jsx
@@ -1,23 +1,33 @@
-import React, { useRef } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import "./field.scss";
 
 function InputField({ filterKeys, useState: inputState, ...other }) {
   const [inputText, setInputText] = inputState;
+  const [currentInputText, setCurrentInputText] = useState("");
+  useEffect(() => {
+    const newCurrentInputText = currentInputText
+      .split("")
+      .filter(key => ![filterKeys].flat(Infinity).includes(key))
+      .join("");
+    if (newCurrentInputText !== currentInputText) {
+      setInputText(newCurrentInputText);
+      setCurrentInputText(newCurrentInputText);
+    }
+  }, [
+    filterKeys,
+    inputText,
+    setInputText,
+    currentInputText,
+    setCurrentInputText
+  ]);
   return (
     <input
       ref={useRef()}
       className="input-field"
       type="text"
       name="input-field"
-      onChange={e =>
-        setInputText(
-          e.target.value
-            .split("")
-            .filter(key => ![filterKeys].flat(Infinity).includes(key))
-            .join("")
-        )
-      }
-      value={inputText}
+      onChange={e => setCurrentInputText(e.target.value)}
+      value={currentInputText}
       {...other}
     />
   );
