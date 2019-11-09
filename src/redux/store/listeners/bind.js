9f31a63fcbe2cf4c733ab7dbc5148f50b0105b59
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2019-11-09 15:48:56 +0100

    Binded navigator listener

--- src/redux/store/listeners/bind.js
+++ src/redux/store/listeners/bind.js
@@ -1,9 +1,11 @@
 import bindWindowListeners from "./listener/window";
 import bindHistoryListener from "./listener/history";
 import bindRouterListener from "./listener/router";
+import bindNavigatorListener from "./listener/navigator";
 
 export default store => {
   bindHistoryListener(store);
   bindRouterListener(store);
   bindWindowListeners(store);
+  bindNavigatorListener(store);
 };
