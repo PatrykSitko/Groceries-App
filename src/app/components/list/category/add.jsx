40ee595c2d8ca2c66920047ba9019b787bfaf132
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2019-11-09 10:58:52 +0100

    Created look, feel and functionality

--- src/app/components/list/category/add.jsx
+++ src/app/components/list/category/add.jsx
@@ -1,20 +1,79 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./add.scss";
+import { getElementRef, getElementRect } from "../../tools/element";
+import InputField from "../../input/field";
+import ConfirmButton from "../../button/confirm";
+import { vmin } from "../../tools/vscale";
+
+const FIRST = 0;
 
 function AddListCategory({
+  useInputTextState,
+  useConfirmedState,
   product,
-  currentLanguage: language,
-  supportedLanguages,
+  style,
   ...other
 }) {
-  const translateLanguages = [supportedLanguages]
-    .flat(Infinity)
-    .filter(supportedLanguage => supportedLanguage !== language);
+  const [inputText, setInputText] = useInputTextState;
+  const [confirmed, setConfirmed] = useConfirmedState;
+  const [categoryRect, setCategoryRect] = useState({});
+  const [updateTimeout, setUpdateTimeout] = useState(undefined);
+  useUpdateCategoryRect(
+    categoryRect,
+    setCategoryRect,
+    updateTimeout,
+    setUpdateTimeout
+  );
   return (
-    <div {...other}>
-      <div className="product-descriptor">{product}</div>
+    <div className="add-category">
+      <InputField
+        className="category-descriptor"
+        useState={[inputText, setInputText]}
+        {...{
+          key: "category-descriptor",
+          style: { ...style, ...categoryRect },
+          ...other
+        }}
+      />
+      <ConfirmButton
+        useState={[confirmed, setConfirmed]}
+        key="confirm-button"
+      />
     </div>
   );
 }
 
+function useUpdateCategoryRect(
+  categoryRect,
+  setCategoryRect,
+  updateTimeout,
+  setUpdateTimeout
+) {
+  useEffect(() => {
+    if (!updateTimeout) {
+      setUpdateTimeout(
+        setTimeout(() => {
+          const categoryRef = getElementRef(
+            document.getElementsByClassName("category")[FIRST]
+          );
+          if (!categoryRef || !categoryRef.current) {
+            return;
+          }
+          const rect = getElementRect(categoryRef);
+          if (!rect) {
+            return;
+          }
+          const { width, height } = rect;
+          if (width !== categoryRect.width || height !== categoryRect.height) {
+            setCategoryRect({
+              width: width - vmin(0.2),
+              height: height - vmin(0.95)
+            });
+          }
+          setUpdateTimeout(clearTimeout(updateTimeout));
+        }, 7)
+      );
+    }
+  }, [categoryRect, setCategoryRect, updateTimeout, setUpdateTimeout]);
+}
 export default AddListCategory;
