a073de595be7c42935e5ee44f95c5c5c6a355ade
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2019-11-11 14:53:17 +0100

    Added no Internet connection notice

--- src/app/components/menu/index.js
+++ src/app/components/menu/index.js
@@ -4,15 +4,17 @@ import { connect } from "react-redux";
 
 import MenuHeader from "./header";
 import MenuFooter, { Entry } from "./footer";
+import OfflineNotice from "../screen/notice/neInternetConnection";
 import images from "../../resources/images";
 
 const mapStateToProps = ({
   state: {
     routes,
-    user: { language }
+    navigator: { onLine },
+    user: { language, name }
   },
   router
-}) => ({ routes, language, router });
+}) => ({ routes, language, userName: name, router, isOnLine: onLine });
 const mapDispatchToProps = dispatch => ({
   changePath: path => dispatch(push(path))
 });
@@ -22,13 +24,16 @@ const appTitle = {
   nl: "BOODSCHAPPEN APP",
   fr: "APP DE COURSES"
 };
-function Menu({ routes, language, router, changePath }) {
+function Menu({ routes, language, router, changePath, isOnLine, userName }) {
   const path = {
     list: `/${language}/${routes[language].list}`,
     graph: `/${language}/${routes[language].graph}`,
     recipes: `/${language}/${routes[language].recipes}`
   };
   return [
+    <OfflineNotice
+      {...{ key: "offline-notice", language, isOnLine, userName }}
+    />,
     <MenuHeader
       {...{
         key: "header",
