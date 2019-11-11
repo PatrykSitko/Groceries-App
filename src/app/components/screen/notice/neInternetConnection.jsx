7a4ff50975e7d3afbd072dc0e0d86877de532072
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2019-11-11 14:52:57 +0100

    Created a no internet connection notice.

---
+++ src/app/components/screen/notice/neInternetConnection.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./noInternetConnection.scss";
+
+const onlineDescriptor = {
+  en: "Dear[name], you're offline.",
+  pl: "Drogi[name], jesteś offline.",
+  fr: "Cher[name], vous êtes hors ligne",
+  nl: "Geachte[name], u bent offline"
+};
+function NoInternetConnectionNotice({ language, userName, isOnLine }) {
+  return (
+    <div className="no-internet-connection-notice" hidden={isOnLine}>
+      {onlineDescriptor[language].replace(
+        "[name]",
+        userName ? ` ${userName}` : ""
+      )}
+    </div>
+  );
+}
+
+export default NoInternetConnectionNotice;
