c54e570c38648e8a9ff9583b262a0bc6de615ade
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2019-11-12 01:57:08 +0100

    Created new product component look and feel

---
+++ src/app/components/list/products/new/product.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import InputField from "../../../input/field";
+import ConfirmButton from "../../../button/confirm";
+import "./product.scss";
+
+function NewProduct({
+  filterKeys,
+  useConfirmNewProductState,
+  useProductNameState,
+  hidden,
+  ...other
+}) {
+  return (
+    <div style={{ display: "flex" }}>
+      <InputField
+        classNamePrefix="new-product"
+        useState={useProductNameState}
+        {...{ filterKeys, hidden, ...other }}
+      />
+      <ConfirmButton
+        classNamePrefix="new-product"
+        useState={useConfirmNewProductState}
+        {...{ hidden }}
+      />
+    </div>
+  );
+}
+
+export default NewProduct;
